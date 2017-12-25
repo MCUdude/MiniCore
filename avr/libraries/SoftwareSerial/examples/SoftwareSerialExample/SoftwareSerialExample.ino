c0d72de72ddb98a71750cfa0ec1de49d28a87056
MCUdude <mcudude@users.noreply.github.com>
2017-12-25 16:00:39 +0100

    Update SoftwareSerial examples

--- avr/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
+++ avr/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
@@ -1,55 +1,91 @@
 /*
-  Software serial multple serial test
+ Software serial multple serial test
 
  Receives from the hardware serial, sends to software serial.
  Receives from software serial, sends to hardware serial.
+ based on Mikal Hart's example
 
  The circuit:
  * RX is digital pin 10 (connect to TX of other device)
  * TX is digital pin 11 (connect to RX of other device)
 
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo and Micro support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
+ Library modified by MCUdude to work with "regular" interrupt pins.
  This example code is in the public domain.
+ 
+ 
+ ***** MIGHTYCORE *****
+ All pins on the ATmega164, ATmega324, ATmega644 and ATmega1284
+ support interrupts, so all pins may be used for RX.
+ 
+ No pins on the ATmega8535, ATmega16 and ATmega32 support
+ pin change interrupts, so only "regular" interrupts may be used.
+ For the "STANDARD pinout" this is digital pin 2, 10, 11
+ For the "BOBUINO pinout" this is digital pin 2, 3, 6
+ 
+ 
+ ***** MEGACORE *****
+ Not all pins on the ATmega640, ATmega1280 and ATmega2560 support interrupts. 
+ For the "Arduino MEGA pinout" the following pins may be used for RX:
+ 2, 3, 10, 11, 12, 13, 18, 19, 20, 21, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+ For the "AVR pinout" the following may be used for RX:
+ 4, 5, 6, 7, 15, 16, 17, 18, 19, 20, 21, 22, 34, 35, 36, 37, 52, 53, 54, 55, 56, 57, 58,
+ 77, 78, 79, 80, 81, 82, 83, 84
+ 
+ Not all pins on the ATmega1281 and ATmega2561 support interrupts.
+ The following pins may be used for RX:
+ 0, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 18, 19, 20, 21
+ 
+ No pins on the ATmega64 and ATmega128 supportpin change interrupts, 
+ so only "regular" interrupts may be used.
+ The following pins may be used for RX:
+ 4, 5, 6, 7, 18, 19, 20, 21
+  
+ 
+ ***** MINICORE *****
+ All pins on the ATmega48, ATmega88, ATmega168 and ATmega328 
+ support pin change interrupts, so all pins may be used for RX.
 
- */
+ No pins on the ATmega8 support pin change interrupts,
+ so only "regular" interrupts may be used. 
+ The following pins may be used for RX: 2, 3
+ 
+ 
+ ***** MAJORCORE *****
+ Not all pins on the ATmega162 support interrupts.
+ The following pins may be used for RX:
+ 10, 11, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
+ 
+ No pins on the ATmega8515 supports pin change interrupts,
+ so only "regular" interrupts may be used. 
+ The following pins may be used for RX: 10, 11, 32
+*/
+
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(10, 11); // RX, TX
 
-void setup() {
+void setup() 
+{
   // Open serial communications and wait for port to open:
   Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
+  while (!Serial); // wait for serial port to connect. Needed for native USB port only 
 
   Serial.println("Goodnight moon!");
 
-  // set the data rate for the SoftwareSerial port
+  // Set the data rate for the SoftwareSerial port
   mySerial.begin(4800);
   mySerial.println("Hello, world?");
 }
 
-void loop() { // run over and over
-  if (mySerial.available()) {
+void loop() 
+{ 
+  if (mySerial.available()) 
+  {
     Serial.write(mySerial.read());
   }
-  if (Serial.available()) {
+  
+  if (Serial.available()) 
+  {
     mySerial.write(Serial.read());
   }
 }
-
--- avr/libraries/SoftwareSerial/examples/TwoPortReceive/TwoPortReceive.ino
+++ avr/libraries/SoftwareSerial/examples/TwoPortReceive/TwoPortReceive.ino
@@ -1,8 +1,9 @@
 /*
-  Software serial multple serial test
+ Software serial multple serial test
 
  Receives from the two software serial ports,
  sends to the hardware serial port.
+ Based on Mikal Hart's twoPortRXExample
 
  In order to listen on a software port, you call port.listen().
  When using two software serial ports, you have to switch ports
@@ -16,23 +17,47 @@
  * First serial device's TX attached to digital pin 10(RX), RX to pin 11(TX)
  * Second serial device's TX attached to digital pin 8(RX), RX to pin 9(TX)
 
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created 18 Apr. 2011
- modified 19 March 2016
- by Tom Igoe
- based on Mikal Hart's twoPortRXExample
-
+ Library modified by MCUdude to work with "regular" interrupt pins.
  This example code is in the public domain.
+ 
+ 
+ ***** MEGACORE *****
+ Not all pins on the ATmega640, ATmega1280 and ATmega2560 support interrupts. 
+ For the "Arduino MEGA pinout" the following pins may be used for RX:
+ 2, 3, 10, 11, 12, 13, 18, 19, 20, 21, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+ For the "AVR pinout" the following may be used for RX:
+ 4, 5, 6, 7, 15, 16, 17, 18, 19, 20, 21, 22, 34, 35, 36, 37, 52, 53, 54, 55, 56, 57, 58,
+ 77, 78, 79, 80, 81, 82, 83, 84
+ 
+ Not all pins on the ATmega1281 and ATmega2561 support interrupts.
+ The following pins may be used for RX:
+ 0, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 18, 19, 20, 21
+ 
+ No pins on the ATmega64 and ATmega128 supportpin change interrupts, 
+ so only "regular" interrupts may be used.
+ The following pins may be used for RX:
+ 4, 5, 6, 7, 18, 19, 20, 21
+  
+ 
+ ***** MINICORE *****
+ All pins on the ATmega48, ATmega88, ATmega168 and ATmega328 
+ support pin change interrupts, so all pins may be used for RX.
 
- */
+ No pins on the ATmega8 support pin change interrupts,
+ so only "regular" interrupts may be used. 
+ The following pins may be used for RX: 2, 3
+ 
+ 
+ ***** MAJORCORE *****
+ Not all pins on the ATmega162 support interrupts.
+ The following pins may be used for RX:
+ 10, 11, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
+ 
+ No pins on the ATmega8515 supports pin change interrupts,
+ so only "regular" interrupts may be used. 
+ The following pins may be used for RX: 10, 11, 32
+*/
+
 
 #include <SoftwareSerial.h>
 // software serial #1: RX = digital pin 10, TX = digital pin 11
@@ -42,27 +67,27 @@ SoftwareSerial portOne(10, 11);
 // on the Mega, use other pins instead, since 8 and 9 don't work on the Mega
 SoftwareSerial portTwo(8, 9);
 
-void setup() {
+void setup() 
+{
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
+  while(!Serial);  // wait for serial port to connect. Needed for native USB port only
 
   // Start each software serial port
   portOne.begin(9600);
   portTwo.begin(9600);
 }
 
-void loop() {
+void loop() 
+{
   // By default, the last intialized port is listening.
   // when you want to listen on a port, explicitly select it:
   portOne.listen();
   Serial.println("Data from port one:");
   // while there is data coming in, read it
   // and send to the hardware serial port:
-  while (portOne.available() > 0) {
+  while (portOne.available() > 0) 
+  {
     char inByte = portOne.read();
     Serial.write(inByte);
   }
@@ -75,7 +100,8 @@ void loop() {
   // while there is data coming in, read it
   // and send to the hardware serial port:
   Serial.println("Data from port two:");
-  while (portTwo.available() > 0) {
+  while (portTwo.available() > 0) 
+  {
     char inByte = portTwo.read();
     Serial.write(inByte);
   }
@@ -83,9 +109,3 @@ void loop() {
   // blank line to separate data from the two ports:
   Serial.println();
 }
-
-
-
-
-
-
