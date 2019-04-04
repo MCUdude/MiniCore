0ad01a2c9cfba7397684eab4b25df0904c1fe05a
MCUdude <hansibull@gmail.com>
2019-04-04 20:19:23 +0200

    File cleanup + add missing macros Closes #71

--- avr/variants/pb-variant/pins_arduino.h
+++ avr/variants/pb-variant/pins_arduino.h
@@ -27,95 +27,106 @@
 #include <avr/pgmspace.h>
 
 
+// Digital pins
+#define NUM_DIGITAL_PINS            (27)
 
-#define NUM_DIGITAL_PINS            27
-#define NUM_ANALOG_INPUTS           8
-#define EXTERNAL_NUM_INTERRUPTS     2
-#define analogInputToDigitalPin(p)  ((p < 5) ? (p) + 14 : ((p < 8) ? (p) + 17) : -1)
-#define analogPinToChannel(p)       ((p) < NUM_ANALOG_INPUTS ? (p) : ((p) >= 14 && (p) < 25) ? (p) - 14 : ((p) >= 25) ? (p) - 19 : -1)
+// PWM pins
 #if defined(__AVR_ATmega48PB__) || defined(__AVR_ATmega88PB__) || defined(__AVR_ATmega168PB__)
 #define digitalPinHasPWM(p)         ((p) == 3 || (p) == 5 || (p) == 6 || (p) == 9 || (p) == 10 || (p) == 11)
 #elif defined(__AVR_ATmega328PB__) 
 #define digitalPinHasPWM(p)         ((p) == 0 || (p) == 1 || (p) == 2 ||(p) == 3 || (p) == 5 || (p) == 6 || (p) == 9 || (p) == 10 || (p) == 11)
 #endif
 
-#define LED_BUILTIN 13
+// Builtin LED
+#define LED_BUILTIN (13)
+static const uint8_t LED = LED_BUILTIN;
 
-static const uint8_t SS   = 10;
-static const uint8_t MOSI = 11;
-static const uint8_t MISO = 12;
-static const uint8_t SCK  = 13;
-static const uint8_t SDA  = 18;
-static const uint8_t SCL  = 19;
+// Analog pins
+#define PIN_A0 (14)
+#define PIN_A1 (15)
+#define PIN_A2 (16)
+#define PIN_A3 (17)
+#define PIN_A4 (18)
+#define PIN_A5 (19)
+#define PIN_A6 (6)
+#define PIN_A7 (7)
+static const uint8_t A0 = PIN_A0;
+static const uint8_t A1 = PIN_A1;
+static const uint8_t A2 = PIN_A2;
+static const uint8_t A3 = PIN_A3;
+static const uint8_t A4 = PIN_A4;
+static const uint8_t A5 = PIN_A5;
+static const uint8_t A6 = PIN_A6;
+static const uint8_t A7 = PIN_A7;
+#define NUM_ANALOG_INPUTS           (8)
+#define analogInputToDigitalPin(p)  ((p < 5) ? (p) + 14 : ((p < 8) ? (p) + 17) : -1)
+#define analogPinToChannel(p)       ((p) < NUM_ANALOG_INPUTS ? (p) : ((p) >= 14 && (p) < 25) ? (p) - 14 : ((p) >= 25) ? (p) - 19 : -1)
 
+// SPI
+#define PIN_SPI_SS    (10)
+#define PIN_SPI_MOSI  (11)
+#define PIN_SPI_MISO  (12)
+#define PIN_SPI_SCK   (13)
+static const uint8_t SS   = PIN_SPI_SS;
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK  = PIN_SPI_SCK;
 #if defined(__AVR_ATmega328PB__)
-static const uint8_t SS0   = 10;
-static const uint8_t MOSI0 = 11;
-static const uint8_t MISO0 = 12;
-static const uint8_t SCK0  = 13;
-static const uint8_t SDA0  = 18;
-static const uint8_t SCL0  = 19;
-
-static const uint8_t MISO1 = 14;
-static const uint8_t SCK1  = 15;
-static const uint8_t SDA1  = 23;
-static const uint8_t SCL1  = 24;
-static const uint8_t SS1   = 25;
-static const uint8_t MOSI1 = 26;
+  #define PIN_SPI_SS0   PIN_SPI_SS
+  #define PIN_SPI_MOSI0 PIN_SPI_MOSI
+  #define PIN_SPI_MISO0 PIN_SPI_MISO
+  #define PIN_SPI_SCK0  PIN_SPI_SCK
+  #define PIN_SPI_SS1   (25)
+  #define PIN_SPI_MOSI1 (26)
+  #define PIN_SPI_MISO1 (14)
+  #define PIN_SPI_SCK1  (15)
+  static const uint8_t SS0   = PIN_SPI_SS0;
+  static const uint8_t MOSI0 = PIN_SPI_MOSI0;
+  static const uint8_t MISO0 = PIN_SPI_MISO0;
+  static const uint8_t SCK0  = PIN_SPI_SCK0;
+  static const uint8_t SS1   = PIN_SPI_SS1;
+  static const uint8_t MOSI1 = PIN_SPI_MOSI1;
+  static const uint8_t MISO1 = PIN_SPI_MISO1;
+  static const uint8_t SCK1  = PIN_SPI_SCK1;
 #endif
 
-static const uint8_t A0 = 14;
-static const uint8_t A1 = 15;
-static const uint8_t A2 = 16;
-static const uint8_t A3 = 17;
-static const uint8_t A4 = 18;
-static const uint8_t A5 = 19;
-static const uint8_t A6 = 25;
-static const uint8_t A7 = 26;
+// i2c
+#define PIN_WIRE_SDA  (18)
+#define PIN_WIRE_SCL  (19)
+static const uint8_t SDA = PIN_WIRE_SDA;
+static const uint8_t SCL = PIN_WIRE_SCL;
+#if defined(__AVR_ATmega328PB__)
+  #define PIN_WIRE_SDA0 PIN_WIRE_SDA
+  #define PIN_WIRE_SCL0 PIN_WIRE_SCL
+  #define PIN_WIRE_SDA1 (23)
+  #define PIN_WIRE_SCL1 (24)
+  static const uint8_t SDA0  = PIN_WIRE_SDA0;
+  static const uint8_t SCL0  = PIN_WIRE_SCL0;
+  static const uint8_t SDA1  = PIN_WIRE_SDA1;
+  static const uint8_t SCL1  = PIN_WIRE_SCL1;
+#endif
 
+// Interrupts
+#define EXTERNAL_NUM_INTERRUPTS     (2)
+#define digitalPinToInterrupt(p) ((p) == 2 ? 0 : ((p) == 3 ? 1 : NOT_AN_INTERRUPT))
+
+// PCINT
 #if defined(__AVR_ATmega48PB__) || defined(__AVR_ATmega88PB__) || defined(__AVR_ATmega168PB__)
 #define digitalPinToPCICR(p)     (((p) >= 0 && (p) <= 22) ? (&PCICR) : ((uint8_t *)0))
 #define digitalPinToPCICRbit(p)  (((p) <= 7) ? 2 : (((p) <= 13 || (p) == 20 || (p) == 21) ? 0 : 1))
 #define digitalPinToPCMSK(p)     (((p) <= 7) ? (&PCMSK2) : (((p) <= 13 || (p) == 20 || (p) == 21) ? (&PCMSK0) : (((p) <= 22) ? (&PCMSK1) : ((uint8_t *)0))))
 #define digitalPinToPCMSKbit(p)  (((p) <= 7) ? (p) : (((p) <= 13) ? ((p) - 8) : ((p) <= 21) ? ((p) - 14) : ((p) - 16)))
-#define digitalPinToInterrupt(p) ((p) == 2 ? 0 : ((p) == 3 ? 1 : NOT_AN_INTERRUPT))
-
 #elif defined(__AVR_ATmega328PB__)
 #define digitalPinToPCICR(p)     (((p) >= 0 && (p) <= 26) ? (&PCICR) : ((uint8_t *)0))
 #define digitalPinToPCICRbit(p)  (((p) <= 7) ? 2 : (((p) <= 13 || (p) == 20 || (p) == 21) ? 0 : (((p) <= 22) ? 1 : 2)))
 #define digitalPinToPCMSK(p)     (((p) <= 7) ? (&PCMSK2) : (((p) <= 13 || (p) == 20 || (p) == 21) ? (&PCMSK0) : (((p) <= 22) ? (&PCMSK1) : (((p) <= 26) ? (&PCMSK3) : ((uint8_t *)0)))))
 #define digitalPinToPCMSKbit(p)  (((p) <= 7) ? (p) : (((p) <= 13) ? ((p) - 8) : ((p) <= 21) ? ((p) - 14) : ((p) == 22) ? ((p) - 16) : ((p) - 23)))
-#define digitalPinToInterrupt(p) ((p) == 2 ? 0 : ((p) == 3 ? 1 : NOT_AN_INTERRUPT))
 #endif
 
-// Make sure the ATmega328PB is backwards compatible with the 328 and 328P
-#if defined(__AVR_ATmega328PB__)
-// SPI
-#define SPCR SPCR0
-#define SPSR SPSR0
-#define SPDR SPDR0
-#define SPI_STC_vect SPI0_STC_vect
-#define SPI_STC_vect_num SPI0_STC_vect_num
-// I2C
-#define TWBR TWBR0
-#define TWSR TWSR0
-#define TWAR TWAR0
-#define TWDR TWDR0
-#define TWCR TWCR0
-#define TWAMR TWAMR0
-#define TWI_vect TWI0_vect
-#define TWI_vect_num TWI0_vect_num
-// UART
-#define USART_RX_vect USART0_RX_vect
-#define USART_RX_vect_num USART0_RX_vect_num
-#define USART_UDRE_vect USART0_UDRE_vect
-#define USART_UDRE_vect_num USART0_UDRE_vect_num
-#define USART_TX_vect USART0_TX_vect
-#define USART_TX_vect_num USART0_TX_vect_num
-#endif
+
+
 
 #ifdef ARDUINO_MAIN
-
 
 // These arrays map port names (e.g. port B) to the
 // appropriate addresses for various functions (e.g. reading
@@ -243,24 +254,32 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
 	NOT_ON_TIMER, // PE3 - D26 / A7
 };
 
-#endif
+#endif // ARDUINO_MAIN
 
-// These serial port names are intended to allow libraries and architecture-neutral
-// sketches to automatically default to the correct port name for a particular type
-// of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
-// the first hardware serial port whose RX/TX pins are not dedicated to another use.
-//
-// SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
-//
-// SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
-//
-// SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
-//
-// SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
-//
-// SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
-//                            pins are NOT connected to anything by default.
-#define SERIAL_PORT_MONITOR   Serial
-#define SERIAL_PORT_HARDWARE  Serial
+// Make sure the ATmega328PB is backwards compatible with the 328 and 328P
+#if defined(__AVR_ATmega328PB__)
+// SPI
+  #define SPCR SPCR0
+  #define SPSR SPSR0
+  #define SPDR SPDR0
+  #define SPI_STC_vect SPI0_STC_vect
+  #define SPI_STC_vect_num SPI0_STC_vect_num
+// I2C
+  #define TWBR TWBR0
+  #define TWSR TWSR0
+  #define TWAR TWAR0
+  #define TWDR TWDR0
+  #define TWCR TWCR0
+  #define TWAMR TWAMR0
+  #define TWI_vect TWI0_vect
+  #define TWI_vect_num TWI0_vect_num
+// UART
+  #define USART_RX_vect USART0_RX_vect
+  #define USART_RX_vect_num USART0_RX_vect_num
+  #define USART_UDRE_vect USART0_UDRE_vect
+  #define USART_UDRE_vect_num USART0_UDRE_vect_num
+  #define USART_TX_vect USART0_TX_vect
+  #define USART_TX_vect_num USART0_TX_vect_num
+#endif // 328PB defs
 
-#endif
+#endif // Pins_Arduino_h
--- avr/variants/standard/pins_arduino.h
+++ avr/variants/standard/pins_arduino.h
@@ -27,7 +27,6 @@
 #include <avr/pgmspace.h>
 
 
-
 // Definitions to make sure all variants will be supported
 #if defined(__AVR_ATmega48__)
 #define __AVR_ATMEGA48A__
@@ -87,37 +86,62 @@
 #endif
 
 
+// Digital pins
+#define NUM_DIGITAL_PINS            (23)
 
-#define NUM_DIGITAL_PINS            23
-#define NUM_ANALOG_INPUTS           8  // 8 for TQFP, 6 for DIP
-#define EXTERNAL_NUM_INTERRUPTS     2
-#define analogInputToDigitalPin(p)  ((p < 5) ? (p) + 14 : ((p < 8) ? (p) + 17) : -1)
-#define analogPinToChannel(p)      ((p) < NUM_ANALOG_INPUTS ? (p) : (p) >= 14 ? (p) - 14 : -1)
-
+// PWM pins
 #if defined(__AVR_ATmega8__)
-#define digitalPinHasPWM(p)         ((p) == 9 || (p) == 10 || (p) == 11)
+  #define digitalPinHasPWM(p)      ((p) == 9 || (p) == 10 || (p) == 11)
 #else
-#define digitalPinHasPWM(p)         ((p) == 3 || (p) == 5 || (p) == 6 || (p) == 9 || (p) == 10 || (p) == 11)
+  #define digitalPinHasPWM(p)      ((p) == 3 || (p) == 5 || (p) == 6 || (p) == 9 || (p) == 10 || (p) == 11)
 #endif
 
-static const uint8_t SS   = 10;
-static const uint8_t MOSI = 11;
-static const uint8_t MISO = 12;
-static const uint8_t SCK  = 13;
+// Builtin LED
+#define LED_BUILTIN (13)
+static const uint8_t LED = LED_BUILTIN;
 
-static const uint8_t SDA = 18;
-static const uint8_t SCL = 19;
-#define LED_BUILTIN 13
+// Analog pins
+#define PIN_A0 (14)
+#define PIN_A1 (15)
+#define PIN_A2 (16)
+#define PIN_A3 (17)
+#define PIN_A4 (18)
+#define PIN_A5 (19)
+#define PIN_A6 (6)
+#define PIN_A7 (7)
+static const uint8_t A0 = PIN_A0;
+static const uint8_t A1 = PIN_A1;
+static const uint8_t A2 = PIN_A2;
+static const uint8_t A3 = PIN_A3;
+static const uint8_t A4 = PIN_A4;
+static const uint8_t A5 = PIN_A5;
+static const uint8_t A6 = PIN_A6;
+static const uint8_t A7 = PIN_A7;
+#define NUM_ANALOG_INPUTS           (8)
+#define analogInputToDigitalPin(p)  ((p < 5) ? (p) + 14 : ((p < 8) ? (p) + 17) : -1)
+#define analogPinToChannel(p)       ((p) < NUM_ANALOG_INPUTS ? (p) : (p) >= 14 ? (p) - 14 : -1)
 
-static const uint8_t A0 = 14;
-static const uint8_t A1 = 15;
-static const uint8_t A2 = 16;
-static const uint8_t A3 = 17;
-static const uint8_t A4 = 18;
-static const uint8_t A5 = 19;
-static const uint8_t A6 = 6;
-static const uint8_t A7 = 7;
+// SPI
+#define PIN_SPI_SS    (10)
+#define PIN_SPI_MOSI  (11)
+#define PIN_SPI_MISO  (12)
+#define PIN_SPI_SCK   (13)
+static const uint8_t SS   = PIN_SPI_SS;
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK  = PIN_SPI_SCK;
 
+// i2c
+#define PIN_WIRE_SDA  (18)
+#define PIN_WIRE_SCL  (19)
+static const uint8_t SDA = PIN_WIRE_SDA;
+static const uint8_t SCL = PIN_WIRE_SCL;
+
+// Interrupts
+#define EXTERNAL_NUM_INTERRUPTS     (2)
+#define digitalPinToInterrupt(p)  ((p) == 2 ? 0 : ((p) == 3 ? 1 : NOT_AN_INTERRUPT))
+
+// PCINT
 #if !defined(__AVR_ATmega8__)
 #define digitalPinToPCICR(p)    (((p) >= 0 && (p) <= 22) ? (&PCICR) : ((uint8_t *)0))
 #define digitalPinToPCICRbit(p) (((p) <= 7) ? 2 : (((p) <= 13) ? 0 : (((p) <= 19) ? 1 : (((p) <= 21) ? 0 : -1))))
@@ -125,35 +149,8 @@ static const uint8_t A7 = 7;
 #define digitalPinToPCMSKbit(p) (((p) <= 7) ? (p) : (((p) <= 13) ? ((p) - 8) : (((p) <= 21) ? ((p) - 14) : (((p) <= 22) ? ((p) - 16) : -1))))
 #endif
 
-#define digitalPinToInterrupt(p)  ((p) == 2 ? 0 : ((p) == 3 ? 1 : NOT_AN_INTERRUPT))
 
 #ifdef ARDUINO_MAIN
-
-// On the Arduino board, digital pins are also used
-// for the analog output (software PWM).  Analog input
-// pins are a separate set.
-
-// AVR ATmega48, ATmega8, ATmega88, ATmega168, ATmega328
-//
-//                    +-\/-+
-//   RST (D 22) PC6  1|    |28  PC5 (A 5)
-//        (D 0) PD0  2|    |27  PC4 (A 4)
-//        (D 1) PD1  3|    |26  PC3 (A 3)
-//        (D 2) PD2  4|    |25  PC2 (A 2)
-//   PWM+ (D 3) PD3  5|    |24  PC1 (A 1)
-//        (D 4) PD4  6|    |23  PC0 (A 0)
-//              VCC  7|    |22  GND
-//              GND  8|    |21  AREF
-// XTAL1 (D 20) PB6  9|    |20  AVCC
-// XTAL2 (D 21) PB7 10|    |19  PB5 (D 13)
-//   PWM+ (D 5) PD5 11|    |18  PB4 (D 12)
-//   PWM+ (D 6) PD6 12|    |17  PB3 (D 11) PWM
-//        (D 7) PD7 13|    |16  PB2 (D 10) PWM
-//        (D 8) PB0 14|    |15  PB1 (D 9)  PWM
-//                    +----+
-//
-// (PWM+ indicates the additional PWM pins on the ATmega48/88/168/328)
-
 
 // These arrays map port names (e.g. port B) to the
 // appropriate addresses for various functions (e.g. reading
@@ -238,14 +235,12 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
 	NOT_ON_TIMER, /* 0 - port D */
 	NOT_ON_TIMER,
 	NOT_ON_TIMER,
-	// on the ATmega168, digital pin 3 has hardware pwm
 #if defined(__AVR_ATmega8__)
 	NOT_ON_TIMER,
 #else
 	TIMER2B,
 #endif
 	NOT_ON_TIMER,
-	// on the ATmega168, digital pins 5 and 6 have hardware pwm
 #if defined(__AVR_ATmega8__)
 	NOT_ON_TIMER,
 	NOT_ON_TIMER,
@@ -276,23 +271,5 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
 };
 
 #endif
-
-// These serial port names are intended to allow libraries and architecture-neutral
-// sketches to automatically default to the correct port name for a particular type
-// of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
-// the first hardware serial port whose RX/TX pins are not dedicated to another use.
-//
-// SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
-//
-// SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
-//
-// SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
-//
-// SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
-//
-// SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
-//                            pins are NOT connected to anything by default.
-#define SERIAL_PORT_MONITOR   Serial
-#define SERIAL_PORT_HARDWARE  Serial
 
 #endif
