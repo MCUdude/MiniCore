c0748564282dfcc897ad6790aae7fd22dd2d7b7d
MCUdude <hansibull@gmail.com>
2023-01-05 23:50:00 +0100

    Remove Wire1_timeout.h The file didn't really serve a purpose anyways, as the Wire1 library depends on Wire, and Wire already includes Wire_timeout.h

--- avr/libraries/Wire1/src/Wire1.h
+++ avr/libraries/Wire1/src/Wire1.h
@@ -2,7 +2,6 @@
 #define TwoWire1_h
 
 #include <Wire.h>
-#include "Wire1_timeout.h"
 
 extern TwoWire Wire1;
 
--- avr/libraries/Wire1/src/Wire1_timeout.h
+++
@@ -1,4 +0,0 @@
-// Enable Wire timeout by uncommenting the line below
-#ifndef WIRE_TIMEOUT
-//#define WIRE_TIMEOUT
-#endif
